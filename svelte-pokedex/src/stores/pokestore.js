f9faa6328fe0a770dffcd92906576240537b6856
Oscar <ocisneros1@csu.fullerton.edu>
2022-10-22 13:49:05 -0700

    ignore files

--- svelte-pokedex/src/stores/pokestore.js
+++ svelte-pokedex/src/stores/pokestore.js
@@ -1,19 +1,22 @@
-import {writable} from "svelte/store";
+import { writable } from "svelte/store";
 
 export const pokemon = writable([]);
+
 const fetchPokemon = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
-    const res = await fetch(url);
-    const data = await res.json();
-    const loadedPokemon = data.results.map((data, index) =>{
-        return {
-            name: data.name,
-            id: index + 1, 
-            image: `http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +1}.png`
-        };
-    });
-    pokemon.set(loadedPokemon);
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const loadedPokemon = data.results.map((data, index) =>{
+    const img_url = `http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +1}.png`;
+    return {
+      name: data.name,
+      id: index + 1, 
+      image: img_url
+    };
+  });
+
+  pokemon.set(loadedPokemon);
 };
 
 fetchPokemon();
-
