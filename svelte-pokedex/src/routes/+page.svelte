a3fbc8be1940fc9d71920d29276f345703d7b596
Oscar <ocisneros1@csu.fullerton.edu>
2022-10-25 21:49:04 -0700

    update

--- svelte-pokedex/src/routes/+page.svelte
+++ svelte-pokedex/src/routes/+page.svelte
@@ -23,12 +23,12 @@
 <h1 class="test-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
 <input
-	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	class="w-8/12 rounded-md text-lg p-4 border-2 border-gray-200"
 	type="text"
 	bind:value={searchTerm}
 	placeholder="Search Pokemon"
 />
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+<div class="lg:px-10 py-4 grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
 	{#each filteredPokemon as pokeman}
 		<PokemanCard {pokeman} />
 	{/each}
